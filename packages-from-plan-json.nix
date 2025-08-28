64fc61e121caf7b4f7fb6d6af1a13fefb5831f4e
bgl gwyng <bgl@gwyng.com>
2025-08-28 17:38:33 +0900

    feat: support custom Hackage repositories by using pkg-src.repo.uri instead of hardcoded URL

--- packages-from-plan-json.nix
+++ packages-from-plan-json.nix
@@ -4,20 +4,21 @@ let
   configureds = builtins.filter (x: x.type == "configured" && x.pkg-src.type == "repo-tar") install-plan;
 in
 builtins.listToAttrs (builtins.map
-  ({ pkg-name, pkg-version, pkg-src-sha256, pkg-cabal-sha256, ... }: {
+  ({ pkg-name, pkg-version, pkg-src-sha256, pkg-cabal-sha256, pkg-src, ... }: {
     name = pkg-name;
     value = {
       source =
         let
-          src-tar-gz = (builtins.fetchurl {
-            url =
-              "https://hackage.haskell.org/package/${pkg-name}-${pkg-version}/${pkg-name}-${pkg-version}.tar.gz";
-            sha256 = pkg-src-sha256;
-          });
+          src-tar-gz = assert pkg-src.repo.type == "remote-repo" || pkg-src.repo.type == "secure-repo";
+            (builtins.fetchurl {
+              url =
+                "${pkg-src.repo.uri}package/${pkg-name}-${pkg-version}/${pkg-name}-${pkg-version}.tar.gz";
+              sha256 = pkg-src-sha256;
+            });
           # FIXME: the fetched metadata is the latest and there's no guarantee it matches the hash
           metadata = (builtins.fetchurl {
             url =
-              "https://hackage.haskell.org/package/${pkg-name}-${pkg-version}/${pkg-name}.cabal";
+              "${pkg-src.repo.uri}package/${pkg-name}-${pkg-version}/${pkg-name}.cabal";
             sha256 = pkg-cabal-sha256;
           });
         in
