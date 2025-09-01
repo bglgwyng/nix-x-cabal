6cd74bd76aff983c91b75c633bff58b6da840d1e
bgl gwyng <bgl@gwyng.com>
2025-09-01 18:08:04 +0900

    feat: add support for source-repo package type

--- packages-from-plan-json.nix
+++ packages-from-plan-json.nix
@@ -1,39 +1,56 @@
 { pkgs, plan-json }:
 let
   install-plan = plan-json.install-plan;
-  configureds = builtins.filter (x: x.type == "configured" && x.pkg-src.type == "repo-tar") install-plan;
+  configureds = builtins.filter (pkg: pkg.type == "configured" && (pkg.pkg-src.type == "repo-tar" || pkg.pkg-src.type == "source-repo")) install-plan;
 in
 builtins.listToAttrs (builtins.map
-  ({ pkg-name, pkg-version, pkg-src-sha256, pkg-cabal-sha256, pkg-src, ... }: {
+  (args@{ pkg-name, pkg-version, pkg-src-sha256, pkg-src, ... }: {
     name = pkg-name;
     value = {
       source =
-        let
-          src-tar-gz = assert pkg-src.repo.type == "remote-repo" || pkg-src.repo.type == "secure-repo";
-            (builtins.fetchurl {
-              url =
-                "${pkg-src.repo.uri}package/${pkg-name}-${pkg-version}/${pkg-name}-${pkg-version}.tar.gz";
+        if pkg-src.type == "repo-tar" then
+          let
+            src-tar-gz = (builtins.fetchurl {
+              url = "${pkg-src.repo.uri}package/${pkg-name}-${pkg-version}/${pkg-name}-${pkg-version}.tar.gz";
               sha256 = pkg-src-sha256;
             });
-          # FIXME: the fetched metadata is the latest and there's no guarantee it matches the hash
-          metadata = (builtins.fetchurl {
-            url =
-              "${pkg-src.repo.uri}package/${pkg-name}-${pkg-version}/${pkg-name}.cabal";
-            sha256 = pkg-cabal-sha256;
-          });
-        in
-        pkgs.stdenv.mkDerivation {
-          name = pkg-name;
-          src = src-tar-gz;
-          installPhase = ''
-            mkdir -p $out
-            cd $out
-            tar -xvf $src --strip-components=1
-            cp ${metadata} ${pkg-name}.cabal
-          '';
-          dontBuild = true;
+            # FIXME: the fetched metadata is the latest and there's no guarantee it matches the hash
+            metadata = (builtins.fetchurl {
+              url =
+                "${pkg-src.repo.uri}package/${pkg-name}-${pkg-version}/${pkg-name}.cabal";
+              sha256 = args.pkg-cabal-sha256;
+            });
+          in
+          pkgs.stdenv.mkDerivation {
+            name = pkg-name;
+            src = src-tar-gz;
+            installPhase = ''
+              mkdir -p $out
+              cd $out
+            
+              tar -xzf $src --strip-components=1
+              ${if pkg-src.type == "source-repo" && pkg-src.source-repo ? "subdir" then
+                "tar -xzf $src --strip-components=1"
+                else "tar -xzf $src --strip-components=1"
+              }
 
-        };
+            
+              ${if pkg-src.type == "repo-tar" then "cp ${metadata} ${pkg-name}.cabal" else ""}
+            '';
+            dontBuild = true;
+          }
+        else
+          assert pkg-src.type == "source-repo";
+          assert pkg-src.source-repo ? "tag";
+          let
+            src =
+              builtins.fetchGit
+                {
+                  url = pkg-src.source-repo.location;
+                  rev = pkg-src.source-repo.tag;
+                };
+          in
+          if pkg-src.source-repo ? "subdir" then "${src}/${pkg-src.source-repo.subdir}" else src;
     };
   })
   configureds
