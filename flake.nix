5da416d1310756b71934a6c1c894dff14afa8d57
bgl gwyng <bgl@gwyng.com>
2025-09-05 14:52:41 +0900

    wip

--- flake.nix
+++ flake.nix
@@ -29,7 +29,74 @@
         };
       flake = {
         generate-plan-json = import ./generate-plan-json.nix;
-        packages-from-plan-json = import ./packages-from-plan-json.nix;
+        package-srcs-from-plan-json = import ./packages-from-plan-json.nix;
+        packages-from-plan-json =
+          { pkgs, plan-json, haskellPackages ? pkgs.haskellPackages }:
+          let
+            install-plan = plan-json.install-plan;
+            configureds =
+              builtins.filter
+                (
+                  pkg:
+                  let
+                    x = pkg.type == "configured"
+                      && (pkg.pkg-src.type == "repo-tar" || pkg.pkg-src.type == "source-repo")
+                      # TODO: exclusive?
+                      # "lib(:.*)?"
+                      && (!(pkg ? "component-name") || (builtins.match "^lib$" pkg.component-name != null))
+                      && (!(pkgs ? "components") || pkg.components ? "lib");
+                  in
+                  # builtins.trace
+                    #   (builtins.toJSON [
+                    #     pkg.pkg-name
+                    #     (pkgs ? "component-name")
+                    #     # ) || builtins.trace pkg (builtins.match "lib(:.*)?" pkg.component-name != null))
+                    #     # (!(pkgs ? "components") || pkgs.components ? "lib")
+                    #   ])
+                  x
+                )
+                install-plan;
+            srcs = builtins.listToAttrs (builtins.map (plan: pkgs.callPackage ./plan-to-source.nix { inherit plan; }) configureds);
+            packages-by-id =
+              # builtins.trace (builtins.toJSON (builtins.map (plan: plan.pkg-name) configureds))
+              (builtins.listToAttrs
+                (builtins.map
+                  (plan:
+                    let src = pkgs.callPackage ./plan-to-source.nix { inherit plan; }; in
+                    {
+                      name = plan.id;
+                      value = { inherit src plan; };
+                    })
+                  configureds));
+            extract-depens = plan:
+              (if plan ? "depends" then plan.depends else [ ])
+              ++ (if plan ? "components" then plan.components.lib.depends else [ ]);
+            override-haskell-packages-in-plan = (name: value:
+              let inherit (value) plan src;
+                xs = builtins.listToAttrs (
+                  builtins.map
+                    (id: { name = packages-by-id.${id}.plan.pkg-name; value = overided-packages-by-id.${id}; })
+                    (builtins.filter
+                      (id: packages-by-id ? "${id}")
+                      (extract-depens plan)));
+              in
+              # (haskellPackages.callCabal2nix plan.pkg-name src { }).override { }
+                # builtins.trace name (builtins.trace (extract-depens plan) (builtins.trace xs 
+                # (
+              pkgs.haskell.lib.dontHaddock
+                (pkgs.haskell.lib.dontCheck
+                  ((haskellPackages.callCabal2nix plan.pkg-name src { }).override (
+                    xs
+                  )))
+              # )))
+            );
+            overided-packages-by-id =
+              builtins.mapAttrs
+                override-haskell-packages-in-plan
+                packages-by-id;
+          in
+          overided-packages-by-id;
+        # builtins.mapAttrs (name: src: (haskellPackages.callCabal2nix name src.source { }).override { }) srcs;
       };
     };
 }
--- plan-to-source.nix
+++ plan-to-source.nix
@@ -2,53 +2,46 @@
 let
   inherit (plan) pkg-name pkg-version pkg-src-sha256 pkg-src;
 in
-{
-  name = pkg-name;
-  value = {
-    inherit plan;
-    source =
-      if pkg-src.type == "repo-tar" then
-        let
-          src-tar-gz = (builtins.fetchurl {
-            url = "${pkg-src.repo.uri}package/${pkg-name}-${pkg-version}/${pkg-name}-${pkg-version}.tar.gz";
-            sha256 = pkg-src-sha256;
-          });
-          # FIXME: the fetched metadata is the latest and there's no guarantee it matches the hash
-          metadata = (builtins.fetchurl {
-            url =
-              "${pkg-src.repo.uri}package/${pkg-name}-${pkg-version}/${pkg-name}.cabal";
-            sha256 = plan.pkg-cabal-sha256;
-          });
-        in
-        pkgs.stdenv.mkDerivation {
-          name = pkg-name;
-          src = src-tar-gz;
-          installPhase = ''
-            mkdir -p $out
-            cd $out
+if pkg-src.type == "repo-tar" then
+  let
+    src-tar-gz = (builtins.fetchurl {
+      url = "${pkg-src.repo.uri}package/${pkg-name}-${pkg-version}/${pkg-name}-${pkg-version}.tar.gz";
+      sha256 = pkg-src-sha256;
+    });
+    # FIXME: the fetched metadata is the latest and there's no guarantee it matches the hash
+    metadata = (builtins.fetchurl {
+      url =
+        "${pkg-src.repo.uri}package/${pkg-name}-${pkg-version}/${pkg-name}.cabal";
+      sha256 = plan.pkg-cabal-sha256;
+    });
+  in
+  pkgs.stdenv.mkDerivation {
+    name = pkg-name;
+    src = src-tar-gz;
+    installPhase = ''
+      mkdir -p $out
+      cd $out
             
-            tar -xzf $src --strip-components=1
-            ${if pkg-src.type == "source-repo" && pkg-src.source-repo ? "subdir" then
-              "tar -xzf $src --strip-components=1"
-              else "tar -xzf $src --strip-components=1"
-            }
+      tar -xzf $src --strip-components=1
+      ${if pkg-src.type == "source-repo" && pkg-src.source-repo ? "subdir" then
+        "tar -xzf $src --strip-components=1"
+        else "tar -xzf $src --strip-components=1"
+      }
 
             
-            ${if pkg-src.type == "repo-tar" then "cp ${metadata} ${pkg-name}.cabal" else ""}
-          '';
-          dontBuild = true;
-        }
-      else
-        assert pkg-src.type == "source-repo";
-        assert pkg-src.source-repo ? "tag";
-        let
-          src =
-            builtins.fetchGit
-              {
-                url = pkg-src.source-repo.location;
-                rev = pkg-src.source-repo.tag;
-              };
-        in
-        if pkg-src.source-repo ? "subdir" then "${src}/${pkg-src.source-repo.subdir}" else src;
-  };
-}
+      ${if pkg-src.type == "repo-tar" then "cp ${metadata} ${pkg-name}.cabal" else ""}
+    '';
+    dontBuild = true;
+  }
+else
+  assert pkg-src.type == "source-repo";
+  assert pkg-src.source-repo ? "tag";
+  let
+    src =
+      builtins.fetchGit
+        {
+          url = pkg-src.source-repo.location;
+          rev = pkg-src.source-repo.tag;
+        };
+  in
+  if pkg-src.source-repo ? "subdir" then "${src}/${pkg-src.source-repo.subdir}" else src
