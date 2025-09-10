564f5ca8e51f1a70e936baaac1efc3d1ce1a59ef
bgl gwyng <bgl@gwyng.com>
2025-09-10 17:49:49 +0900

    refactor: simplify flake.nix by removing unused perSystem block and cleaning imports

--- flake.nix
+++ flake.nix
@@ -9,21 +9,8 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       debug = true;
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
-        (import ./flakeModule.nix)
-      ];
+      imports = [ (import ./flakeModule.nix) ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }:
-        let inherit (pkgs) lib haskell; in
-        {
-          # Per-system attributes can be defined here. The self' and inputs'
-          # module parameters provide easy access to attributes of the same
-          # system.
-        };
       flake = {
         flakeModule = import ./flakeModule.nix;
       };
