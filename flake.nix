f981e8d50ef5afbec2b94b9b7516f0405fa60cee
bgl gwyng <bgl@gwyng.com>
2025-09-16 14:56:34 +0900

    wip

--- flake.nix
+++ flake.nix
@@ -7,12 +7,33 @@
     nix-x-cabal-utils.url = "github:bglgwyng/nix-x-cabal-utils";
   };
 
-  outputs = inputs@{ flake-parts, nix-x-cabal-utils, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      debug = true;
+  outputs = inputs@{ flake-parts, nixpkgs, nix-x-cabal-utils, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({ flake-parts-lib, ... }: {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       flake = {
         flakeModule = import ./flakeModule.nix { inherit nix-x-cabal-utils; };
       };
-    };
+      perSystem = { config, self', system, pkgs, lib, ... }: {
+        packages = {
+          docs =
+            let
+              # Import the flake module to extract options
+              flakeModule = import ./flakeModule.nix { inherit (inputs) nix-x-cabal-utils; };
+
+              eval = lib.evalModules {
+                specialArgs = { inherit pkgs flake-parts-lib; };
+                modules = [
+                  flakeModule
+                ];
+              };
+              optionsDoc = pkgs.nixosOptionsDoc {
+                inherit (eval) options;
+              };
+            in
+            pkgs.runCommand "options-doc.md" { } ''
+              cat ${optionsDoc.optionsCommonMark} > $out
+            '';
+        };
+      };
+    });
 }
